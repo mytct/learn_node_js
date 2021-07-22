21a941f3cf21b14885c3e81024bb3887ccacbdbe
mytran <mtoanmy91@gmail.com>
2021-07-21 20:19:22 -0700

    - add static -files

--- static_file.js
+++ static_file.js
@@ -3,8 +3,18 @@ var http = require('http'),
 
 function serveStaticFile(res, path, contentType, responseCode) {
     if(!responseCode) responseCode = 200;
+    //__dirname will resolve to the directory the executing script resides in
+    //    So if your script resides in /home/sites/app.js, __dirname will resolve
+    //    to /home/sites
+    
+    //readFile is an asynchronous method for reading files, There is a synchronous
+    //  version of that function, fs.readFileSync, but the sooner you start thinking asyn‐
+    //  chronously, the better
     fs.readFile(__dirname + path, function(err, data){
         if(err) {
+            //if the file didn’t exist or there were permissions issues reading the file, the err
+            // variable is set, and the function returns an HTTP status code of 500 indicating a server
+            // error
             res.writeHead(500, { 'Content-Type' : 'text/plain' });
             res.end('500 - Internal Error');
         } else {
